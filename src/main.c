489a8a34317eeee8d4b8b91d761a268815e076f1
joao <83374960+J-joao@users.noreply.github.com>
2021-11-29 13:34:41 -0300

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* error declaration */
+#define BADUSAGE "\e[0;91m[error]\e[0m Wrong arguments suplied!\n\e[0;94mUsage\e[0m: repeat <times> <string>\n"
+#define PIPEBADUSAGE "\e[0;91m[error]\e[0m Wrong arguments suplied!\n\e[0;94mUsage\e[0m: program <statement> | repeat <times>\n"
+
+int main(int argc, const char *argv[])
+{
+    char str[10000] = {'\0'};
+    FILE* fd = stdin;
+
+    /* if connected to the terminal, take string from argv[2] */
+    if(isatty(fileno(fd))) 
+    {
+        if (argc <= 1 || argc > 3) 
+        {
+            fprintf(stderr, BADUSAGE);
+            exit(EXIT_FAILURE);
+        }
+        strcpy(str, argv[2]);
+    }
+
+    /* if coming from pipe, take string from stdin */
+    else {
+        if (argc <= 1 || argc > 2) 
+        {
+            fprintf(stderr, PIPEBADUSAGE);
+            exit(EXIT_FAILURE);
+        }
+        fgets(str, 1024, stdin);
+    }
+
+    for (unsigned long int i = 0; i < strtoul(argv[1], NULL, 10); i++) { printf("%s", str); }
+
+    return 0;
+}
